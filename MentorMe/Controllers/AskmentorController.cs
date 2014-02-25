213317700840778ab3b44d36e1e325afc8df27fc
HarishankarSomasundaram <harishankar.info@gmail.com>
2014-02-26 01:31:42 +0530

    Initial Commit - v0.2

--- MentorMe/Controllers/AskmentorController.cs
+++ MentorMe/Controllers/AskmentorController.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MentorMe.Controllers
 {
-    public class AskmentorController
+    public class AskmentorController : Controller
     {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
     }
 }
